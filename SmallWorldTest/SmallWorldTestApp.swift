ab48b2b555852e898078ac308b819566e452b3bc
Sabih Haris <sabih.haris@avanzasolutions.com>
2023-12-10 13:30:15 +0500

    Initial Commit

---
+++ SmallWorldTest/SmallWorldTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallWorldTestApp.swift
+//  SmallWorldTest
+//
+//  Created by Sabih Haris on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SmallWorldTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
